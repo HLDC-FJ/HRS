165bc8ca5814d7f286ec5ee77914a7d0e78004c5
yoshio-fujinuki <yoshio-fujinuki@hldc.co.jp>
2022-10-24 09:29:14 +0900

    Bug Fix

---
+++ HRS/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace HRS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpHToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.radarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.r7ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.r8AToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pTToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.vToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.portToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.versionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileFToolStripMenuItem,
+            this.setupToolStripMenuItem,
+            this.toolTToolStripMenuItem,
+            this.helpHToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(609, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileFToolStripMenuItem
+            // 
+            this.fileFToolStripMenuItem.Name = "fileFToolStripMenuItem";
+            this.fileFToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.fileFToolStripMenuItem.Text = "File(F)";
+            // 
+            // setupToolStripMenuItem
+            // 
+            this.setupToolStripMenuItem.Checked = true;
+            this.setupToolStripMenuItem.CheckOnClick = true;
+            this.setupToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.setupToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.radarToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.portToolStripMenuItem});
+            this.setupToolStripMenuItem.Name = "setupToolStripMenuItem";
+            this.setupToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.setupToolStripMenuItem.Text = "Setup(S)";
+            // 
+            // helpHToolStripMenuItem
+            // 
+            this.helpHToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.versionToolStripMenuItem});
+            this.helpHToolStripMenuItem.Name = "helpHToolStripMenuItem";
+            this.helpHToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.helpHToolStripMenuItem.Text = "Help(H)";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 450);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(609, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // radarToolStripMenuItem
+            // 
+            this.radarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.r7ToolStripMenuItem,
+            this.r8AToolStripMenuItem});
+            this.radarToolStripMenuItem.Name = "radarToolStripMenuItem";
+            this.radarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.radarToolStripMenuItem.Text = "Radar";
+            // 
+            // r7ToolStripMenuItem
+            // 
+            this.r7ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pTToolStripMenuItem,
+            this.vToolStripMenuItem,
+            this.tToolStripMenuItem});
+            this.r7ToolStripMenuItem.Name = "r7ToolStripMenuItem";
+            this.r7ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.r7ToolStripMenuItem.Text = "R7";
+            // 
+            // pTToolStripMenuItem
+            // 
+            this.pTToolStripMenuItem.Name = "pTToolStripMenuItem";
+            this.pTToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pTToolStripMenuItem.Text = "PT";
+            // 
+            // vToolStripMenuItem
+            // 
+            this.vToolStripMenuItem.Name = "vToolStripMenuItem";
+            this.vToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.vToolStripMenuItem.Text = "V";
+            // 
+            // tToolStripMenuItem
+            // 
+            this.tToolStripMenuItem.Name = "tToolStripMenuItem";
+            this.tToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.tToolStripMenuItem.Text = "T";
+            // 
+            // r8AToolStripMenuItem
+            // 
+            this.r8AToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pTToolStripMenuItem1,
+            this.vToolStripMenuItem1});
+            this.r8AToolStripMenuItem.Name = "r8AToolStripMenuItem";
+            this.r8AToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.r8AToolStripMenuItem.Text = "R8A";
+            // 
+            // pTToolStripMenuItem1
+            // 
+            this.pTToolStripMenuItem1.Name = "pTToolStripMenuItem1";
+            this.pTToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.pTToolStripMenuItem1.Text = "PT";
+            // 
+            // vToolStripMenuItem1
+            // 
+            this.vToolStripMenuItem1.Name = "vToolStripMenuItem1";
+            this.vToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.vToolStripMenuItem1.Text = "V";
+            // 
+            // portToolStripMenuItem
+            // 
+            this.portToolStripMenuItem.Name = "portToolStripMenuItem";
+            this.portToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.portToolStripMenuItem.Text = "Port";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // toolTToolStripMenuItem
+            // 
+            this.toolTToolStripMenuItem.Name = "toolTToolStripMenuItem";
+            this.toolTToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.toolTToolStripMenuItem.Text = "Tool(T)";
+            // 
+            // versionToolStripMenuItem
+            // 
+            this.versionToolStripMenuItem.Name = "versionToolStripMenuItem";
+            this.versionToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.versionToolStripMenuItem.Text = "Version";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 472);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "60GHz Radar HRS Series";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpHToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripMenuItem radarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem r7ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem r8AToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pTToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem vToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem portToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem versionToolStripMenuItem;
+    }
+}
+
